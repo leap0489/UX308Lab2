9ed3d816c1179e942c5eca4e71a1c0b25dc31247
leap0489 <144965129+leap0489@users.noreply.github.com>
2025-01-17 01:41:39 +0000

    add grade.js

---
+++ grade.js
@@ -0,0 +1,20 @@
+//This program *calculates* a letter grade from a **numerical grade**
+
+let numericalGrade = 92;
+
+if (numericalGrade<60) {
+    console.log(`A mark of ${numericalGrade} % is an F`)}
+
+else if (numericalGrade<70 &&numericalGrade >60) {
+    console.log(`A mark of ${numericalGrade} % is a D`)}
+
+else if (numericalGrade<80 &&numericalGrade >70) {
+    console.log(`A mark of ${numericalGrade} % is a C`)}
+
+else if (numericalGrade<90 &&numericalGrade >80) {
+    console.log(`A mark of ${numericalGrade} % is a B`)}
+
+else if (numericalGrade<100 &&numericalGrade >90) {
+     console.log(`A mark of ${numericalGrade} % is an A`)}
+
+
