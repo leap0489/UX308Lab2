969266fe82031e6d9b7c547dbe3597a18c78e75e
leap0489 <144965129+leap0489@users.noreply.github.com>
2025-01-17 01:11:20 +0000

    Add km2miles and cylinder

---
+++ cylinder.js
@@ -0,0 +1,9 @@
+//This program *calculates* volume of a cylinder from **diameter**
+
+let diameter = 5;
+
+let height = 5;
+
+let volume = (3.14*diameter**2*height)/4;
+
+console.log(`a cylinder with a diameter of ${diameter} has a volume of ${volume}`)
