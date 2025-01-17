3a689c4831ef20482381cf5322199e3fc6418749
leap0489 <144965129+leap0489@users.noreply.github.com>
2025-01-17 01:15:21 +0000

    update cylinder.js

--- cylinder.js
+++ cylinder.js
@@ -1,9 +1,9 @@
 //This program *calculates* volume of a cylinder from **diameter**
 
-let diameter = 5;
+let diameter = 1;
 
-let height = 5;
+let height = 2;
 
-let volume = (3.14*diameter**2*height)/4;
+let volume = (Math.PI*diameter**2*height)/4;
 
-console.log(`a cylinder with a diameter of ${diameter} has a volume of ${volume}`)
+console.log(`a cylinder with a diameter of ${diameter} centimeters has a volume of ${volume} centimeters squared`)
